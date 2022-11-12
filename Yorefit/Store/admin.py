670198d8eb92e2e969553681ca9ab04462e2ab4f
nikkahkarado <118058144+nikkahkarado@users.noreply.github.com>
2022-11-12 13:55:56 +0000

    Delete Yorefit directory

--- Yorefit/Store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product, ProductImage, Order, OrderUpdate, Review
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(Order)
-admin.site.register(OrderUpdate)
-admin.site.register(Review)
